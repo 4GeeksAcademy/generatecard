daeab6379b5ee7d0aa37b88180c4959ab408ecb2
Cesar Contreras <cesar.alberto2003@outlook.com>
2024-08-22 15:43:30 +0000

    Simple random card

--- src/app.js
+++ src/app.js
@@ -7,5 +7,51 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function random(lista) {
+    let randomNum = Math.floor(Math.random() * lista.length);
+    return lista[randomNum];
+  }
+  let symbol = ["♦︎", "♥︎", "♣︎", "♠︎"];
+  let number = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let symbolElement = document.querySelector("#symbols");
+  let numberCardelement = document.querySelector("#cardnum");
+  let symbolCardlow = document.querySelector("#symbolsLow");
+  symbolCardlow.style.transform = "rotate(180deg)";
+
+  const renderNewRandomCard = () => {
+    let symbolCard = random(symbol);
+    let numberCard = random(number);
+
+    symbolElement.innerHTML = symbolCard;
+    numberCardelement.innerHTML = numberCard;
+    symbolCardlow.innerHTML = symbolCard;
+
+    if (symbolCard == "♥︎" || symbolCard == "♦︎") {
+      symbolElement.style.color = "red";
+      symbolCardlow.style.color = "red";
+    } else {
+      symbolElement.style.color = "black";
+      symbolCardlow.style.color = "black";
+    }
+  };
+  renderNewRandomCard();
+
+  let generateRandombtn = document.querySelector("#generate-button");
+
+  generateRandombtn.addEventListener("click", renderNewRandomCard);
 };
